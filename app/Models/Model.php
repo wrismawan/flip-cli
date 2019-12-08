b83eb633e94d422f3c1c23fa443cab613a9e44e8
Wahyu Rismawan <wahyurismawan@gmail.com>
2019-12-08 21:53:26 +0700

    add method UPADATE by Id at Model

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -21,12 +21,39 @@ class Model
 
     public function save($params)
     {
+
         $sql = sprintf(
             'insert into %s (%s) values (%s)',
             $this->table,
             implode(', ', array_keys($params)),
             ':' . implode(', :', array_keys($params))
         );
+
+        try {
+            $statement = $this->connection->prepare($sql);
+            $statement->execute($params);
+        } catch (\Exception $e) {
+            echo "ERROR: " . $e->getMessage() . "\n";
+        }
+    }
+
+    public function updateById($id, $params)
+    {
+        # convert key=>value php array format to "key=:key" format
+        $paramUpdateFormat = implode(', ', array_map(
+            function ($value, $key) { return sprintf("%s=:%s", $key, $key); },
+            $params,
+            array_keys($params)
+        ));
+
+        $sql = sprintf(
+            'update %s set %s where id = %s',
+            $this->table,
+            $paramUpdateFormat,
+            $id
+        );
+
+        echo "\nUPDATE SQL: " . $sql . "\n";
 
         try {
             $statement = $this->connection->prepare($sql);
