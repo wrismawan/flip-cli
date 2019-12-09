ecfdfd2c725afeaef68c53f360dc1314fd0e4370
Wahyu Rismawan <wahyurismawan@gmail.com>
2019-12-09 08:20:45 +0700

    update message at help command

--- app/Controllers/HelpController.php
+++ app/Controllers/HelpController.php
@@ -11,7 +11,24 @@ class HelpController extends CommandController
         $message =
             "Usage: php flip [COMMAND]
             \nCommand list:
-            \n1. hello [YOURNAME]\tWelcome Message for testing purpose";
+            \n1. hello [YOURNAME]
+            > Welcome Message for testing purpose
+            > eg: php flip hello 'wahyu rismawan'
+            
+            \n2. disbursement show [PARAMETERS]
+            > Get disbursement data from API by Id, then update the data if any change.
+            > Parameters:
+            \t--id\t : Dirsbursement Id
+            > Eg: php flip disbursement show --id=123
+            
+            \n3. disbursement send [PARAMETERS]
+            > Send request disbursement data.
+            > Parameters:
+            \t--bank_code\t\t : Bank Code
+            \t--account_number\t : Account Number
+            \t--amount\t\t : Amount to transfer
+            \t--remark\t\t : Remark disbursement
+            Eg: php flip disbursement send --bank_code=021 --account_number=111-222-333 --amount=99999 --remark=\"sample remark\"";
 
         $this->display($message);
     }
