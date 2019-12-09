5a10007d604967d41df67d0219f3efe2ed075a00
Wahyu Rismawan <wahyurismawan@gmail.com>
2019-12-09 14:38:51 +0700

    update config.php

--- config.php
+++ config.php
@@ -7,9 +7,9 @@ return [
     ],
 
     'database' => [
-        'db_name' => 'flip_cli',
-        'db_username' => 'root',
-        'db_password' => 'bismillah',
+        'db_name' => '[YOU-DB-NAME]',
+        'db_username' => '[YOUR-DB-USERNAME]',
+        'db_password' => '[YOUR-DB-PASSWORD]',
         'db_connection' => 'mysql:host=127.0.0.1'
     ]
 ];
